e0499183a71e474011089446c0e1005386b3df35
BryantChi <bryantchi.work@gmail.com>
2025-02-24 03:17:04 +0800

    [ADD] add Laravel Telescope

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
         InfyOm\Generator\InfyOmGeneratorServiceProvider::class,
         InfyOm\GeneratorBuilder\GeneratorBuilderServiceProvider::class,
     ])->toArray(),
