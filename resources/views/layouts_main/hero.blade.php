78804fdbdf31db2d61885122ac3082610097ef38
BryantChi <bryantchi.work@gmail.com>
2025-02-24 11:40:01 +0800

    [MOD] hero ui 調整

--- resources/views/layouts_main/hero.blade.php
+++ resources/views/layouts_main/hero.blade.php
@@ -1,5 +1,5 @@
 <!-- Hero Section -->
-<section id="hero" class="hero section light-background">
+<section id="hero" class="hero section light-background" style="background-image: url({{ asset('assets/images/Banner/1.png') }})" data-aos="fade-in" data-aos-delay="100">
 
     <div class="container">
         <div class="row gy-4 justify-content-center justify-content-lg-between">
